73d9085ab02b42aeddb679d197736fa3c4efa3fb
Pierre Gambarotto <pierre.gambarotto@n7.fr>
2013-02-12 10:43:29 +0100

    person using authentication module

--- person.rb
+++ person.rb
@@ -1,22 +1,14 @@
-require 'digest/sha1'
+require_relative './authentication'
 class Person
    attr_accessor :first_name, :last_name, :id
-   attr_reader :password
 
+  include Authentication
   def initialize
     @first_name = @last_name = @id = ""
   end
 
   def valid?
     not (last_name.nil?|| last_name.empty? || first_name.nil? || first_name.empty? || id.nil? || id.empty?)
-  end
-
-  def password=(clear_text)
-    @password = Digest::SHA1.hexdigest(clear_text)
-  end
-
-  def authenticate(clear_text)
-    Digest::SHA1.hexdigest(clear_text) == password
   end
 
 end
--- spec/person_spec.rb
+++ spec/person_spec.rb
@@ -5,6 +5,7 @@ $:.unshift File.join(File.dirname(__FILE__), '..')
 # File.join : construit un chemin d'accès à un fichier en concaténant les éléments passés
 
 require 'person'
+require 'authentication'
 describe Person do
   context "when created" do
     its(:last_name) {should be_empty}
@@ -40,35 +41,5 @@ describe Person do
     end
   end
 
-  it "should encrypt the password with sha1" do
-    Digest::SHA1.should_receive(:hexdigest).with("foo").and_return("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33")
-    subject.password="foo"
-  end
-
-  it "should store she sha1 digest" do
-    subject.password="foo"
-    subject.password.should == "0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33"
-  end
-
-  describe "user authentication"  do 
-    it "should crypt the clear password given" do 
-      Digest::SHA1.should_receive(:hexdigest).with("foo").and_return("0beec7b5ea3f0fdbc95d0dd47f3c5bc275da8a33")
-      subject.authenticate("foo")
-    end
-
-    describe "authentication with password challenge" do
-      subject{ p = Person.new; p.password = "foo"; p}
-      context "correct password" do
-        it "should return valid authentication" do 
-          subject.authenticate("foo").should be_true
-        end
-      end
-
-      context "incorrect password" do
-        it "should return invalid authentication" do
-          subject.authenticate("bad pass").should be_false
-        end
-      end
-    end
-  end
+  it { should be_a_kind_of Authentication }
 end
